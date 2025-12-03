f8192d95743a7c252ab573210884f1d9c454e54b
fahrulrzi <muhamadfahrulrazi@mail.ugm.ac.id>
2025-12-03 21:38:41 +0700

    feat: update seeder and database for affiliate

--- src/seeders/categories.seeder.ts
+++ src/seeders/categories.seeder.ts
@@ -6,82 +6,82 @@ export const categorySeeder = async () => {
     {
       name: "Shampoo Bar",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/2b21e472-9aa5-43e3-8aee-476c01a90899.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Clarifying Shampoo",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/5f3f6f6c-3f7e-4f12-9f12-6e2f5e4e4b8b.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Face",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/face-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Perfume & Candles",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/62e254fc-a9cb-48da-a7d2-381680304db4.png",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Lotion & Butter",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/lotion-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Solid Soap",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/solid-soap-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Conditioner",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/conditioner-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Daily Light",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/daily-light-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Perfume Balm",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/perfume-balm-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Bath Oil",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/bath-oil-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Soap",
       slug: "soap",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/solid-soap-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Shampoo",
       slug: "shampoo",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/shampoo-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Lotion",
       slug: "lotion",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/lotion-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Perfume",
       slug: "perfume",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/perfume-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
     {
       name: "Makeup",
       slug: "makeup",
       imageUrl:
-        "https://pub-d843a2ecaefc44d1b76d8109ceebcfe3.r2.dev/products/makeup-category.jpg",
+        "https://pbs.twimg.com/media/GcwRlkZbcAMCzRJ.jpg",
     },
   ];
 
